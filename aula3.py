247fc7a027f80fcfc94da2b16b99c1c46c3d603b
Mateus Theodoro <61423297+MateusTheodoro@users.noreply.github.com>
2022-12-02 23:20:56 -0300

    Comentários e Tipos

---
+++ aula3.py
@@ -0,0 +1,23 @@
+
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Nome Sobrenome')
+print(1, 'Nome "Sobrenome"')
+
+# Aspas duplas
+print("Nome Sobrenome")
+print(2, "Nome 'Sobrenome'")
+
+# Escape
+print("Nome \"Sobrenome\"")
+
+# r
+print(r"Nome \"Sobrenome\"")
