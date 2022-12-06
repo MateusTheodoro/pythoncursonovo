fa7025ee4dd771b0b339a40d62285e89a502a605
Mateus Theodoro <61423297+MateusTheodoro@users.noreply.github.com>
2022-12-06 11:06:30 -0300

    Create aula12 - imc.py

---
+++ aula12 - imc.py
@@ -0,0 +1,8 @@
+nome = 'Mateus'
+altura = 1.80
+peso = 95
+imc = peso / (altura * altura)
+
+print(nome + ', voce tem ' + str(altura) + ' de altura,')
+print('pesa ' + str(peso) + ' quilos e seu IMC eh: ')
+print(imc)
