18d914861db5812a669c013d285ba2c6e2e62c81
Mateus Theodoro <61423297+MateusTheodoro@users.noreply.github.com>
2022-12-07 10:03:36 -0300

    introducao f strings

--- aula12 - imc.py
+++ aula12 - imc.py
@@ -3,6 +3,8 @@ altura = 1.80
 peso = 95
 imc = peso / (altura * altura)
 
+imc_formatado = f'{imc:.2f}'
+
 print(nome + ', voce tem ' + str(altura) + ' de altura,')
 print('pesa ' + str(peso) + ' quilos e seu IMC eh: ')
-print(imc)
+print(imc_formatado)
