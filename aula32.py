99a0c95ca954ce851c0781c54355caf31467e48c
Mateus Theodoro <61423297+MateusTheodoro@users.noreply.github.com>
2022-12-21 22:44:33 -0300

    Create aula32.py

---
+++ aula32.py
@@ -0,0 +1,36 @@
+
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+"""
+# ------- 01
+# numero_usuario = input('Digite um número inteiro: ')
+# numero_inteiro = numero_usuario.isnumeric()
+
+# if numero_inteiro:
+
+#     numero_inteiro = int(numero_usuario)
+#     if numero_inteiro % 2 == 0:
+#         print('Seu número é par.')
+#     else:
+#         print('Seu nome é ímpar')
+        
+# else:
+#     print('Você não digitou um número inteiro')
+
+# --------------
+
+# ------- 02
