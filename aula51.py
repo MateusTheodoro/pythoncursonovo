9e6a74fce3a963a6c54d22d023f371bec38ef578
Mateus Theodoro <mateus-jau@hotmail.com>
2023-01-24 11:37:26 -0300

    empacotamento, desempacotamento, tuplas

---
+++ aula51.py
@@ -0,0 +1,12 @@
+"""
+Introdução ao empacotamento e desempacotamento
+"""
+_, _, nome, *resto = ['Maria', 'Helena', 'Luiz']
+print(nome)
+
+"""
+tuplas
+"""
+
+nomes = 'Joao', 'Maria', 'Arroz'
+print(nomes)
